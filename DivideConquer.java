ab89f40c5780d8be3c0eab8f63229c654c6b8835
Kritika Singh <kritikasingh2736@gmail.com>
2025-10-04 19:58:24 +0530

    mergesort for string arrray and majority element in the array

--- DivideConquer.java
+++ DivideConquer.java
@@ -6,6 +6,12 @@ public class DivideConquer {
         }
     }
 
+    public static void printStringArray(String words[]){
+        for(int i=0; i<words.length; i++){
+        System.out.print(words[i] + " ");
+        }
+    }
+
     public static int[] readArray(Scanner sc){
         System.out.print("How many elements do you want to add in the Array: ");
         int n = sc.nextInt();
@@ -19,6 +25,22 @@ public class DivideConquer {
             numbers[i] = sc.nextInt();
         }
         return numbers;
+    }
+
+    public static String[] readSringArray(Scanner sc){
+        System.out.print("How many elements do you want to add in the words Array: ");
+        int n = sc.nextInt();
+
+        if(n <= 0){
+            return new String[0];
+        }
+
+        String words[] = new String[n];
+        for(int i = 0; i < n; i++){
+            System.out.print("Enter word: ");
+            words[i] = sc.next();
+        }
+        return words;
     }
 
     public static void mergeSort(int arr[], int si, int ei){
@@ -127,12 +149,84 @@ public class DivideConquer {
         }
     }
 
+    public static void stringMergeSort(String words[], int si, int ei){
+        if(si>=ei){
+            return;
+        }
+        int mid = si + (ei - si)/2;
+        stringMergeSort(words, si, mid);
+        stringMergeSort(words, mid+1, ei);
+        stringMerge(words, si, mid, ei);
+    }
+    public static void stringMerge(String words[] , int si, int mid, int ei){
+        String temp[] = new String[ei - si +1];
+
+        int i = si;
+        int j = mid + 1;
+        int k = 0;
+
+        while(i<=mid && j<=ei){
+            if(words[i].compareTo(words[j]) < 0){
+                temp[k] = words[i];
+                i++;
+            } else{
+                temp[k] = words[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i<=mid){
+            temp[k++] = words[i++];
+        }
+
+        while(j<=ei){
+            temp[k++] = words[j++];
+        }
+
+        for( k = 0, i = si; k < temp.length; k++, i++){
+            words[i] = temp[k];
+        }
+    }
+
+    private static int countInRange(int nums[], int num, int si, int ei){
+        int count = 0;
+        for(int i=si; i<=ei; i++){
+            if(nums[i] == num){
+                count++;
+            }
+        }
+        return count;
+    }
+    private static int majorityElementRec(int nums[], int si, int ei){
+        if(si == ei){
+            return nums[si];
+        }
+        int mid = si + (ei-si)/2;
+
+        int left = majorityElementRec(nums, si, mid);
+        int right = majorityElementRec(nums, mid+1, ei);
+
+        if(left == right){
+            return left;
+        }
+        int leftCount = countInRange(nums, left , si, ei);
+        int rightCount = countInRange(nums, right, si, ei);
+
+        return leftCount > rightCount ? left : right ;
+    }
+    public static int majorityElement(int nums[]){
+        return majorityElementRec(nums, 0, nums.length-1);
+    }
+
     public static void main(String[] args) {
         Scanner Sc = new Scanner(System.in);
         System.err.println("---------- Divide and Conquer ----------");
         System.out.println("1. Merge Sort");
         System.out.println("2. Quick Sort");
         System.out.println("3. Search in a Sorted and rotated array (variation of Binary search)");
+        System.out.println("4. Merge sort for an array of Strings");
+        System.out.println("5. Find the majority element of an array");
         System.out.print("Enter what you want to perform: ");
         int a = Sc.nextInt();
 
@@ -158,17 +252,45 @@ public class DivideConquer {
                 quickSort(arr, 0, arr.length-1);
                 System.out.print("Sorted array: ");
                 printArray(arr);
+
                 break;
 
             case 3:
-                int snrarr[] = {4,5,6,7,0,1,2};
-                int idx = Search(snrarr, 0 , 0 , snrarr.length-1);
+                System.out.println("Enter a Sorted and Rotated Array");
+                int snrarr[] = readArray(Sc);
+                System.out.print("The Array you entered: ");
+                printArray(snrarr);
+
+                System.out.println("");
+                System.out.print("Enter Target: ");
+                int target = Sc.nextInt();
+                int idx = Search(snrarr, target , 0 , snrarr.length-1);
                 if(idx == -1){
                     System.out.println("Target not found");
                 }else{
                     System.out.println("Target found at index " + idx);
                 }
+
+                break;
+
+            case 4:
+                String words[] = readSringArray(Sc);
+
+                System.out.print("The Array you enterd: ");
+                printStringArray(words);
+                System.out.println("");
+
+                stringMergeSort(words, 0, words.length-1);
+                System.out.print("Sorted Array: ");
+                printStringArray(words);
+
+                break;
         
+            case 5:
+                int nums[] = readArray(Sc);
+                int result = majorityElement(nums);
+                System.out.println("The majority element in the given array is: " + result);
+
             default:
                 break;
         }
