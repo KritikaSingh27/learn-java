ff0ea475059ccc85a6500f5b08287800efb7a300
Kritika Singh <kritikasingh2736@gmail.com>
2025-10-13 23:43:20 +0530

    started backtracking chapter

---
+++ Backtracking/findSubsets.java
@@ -0,0 +1,26 @@
+package Backtracking;
+
+public class findSubsets {
+    public static void findSubsets(String str, String ans, int i){
+        //base case
+        if(i == str.length()){
+            if(str == ""){
+                System.out.println("null");
+            }
+            else{
+                System.out.println(ans);
+            }
+            return;
+        }
+        //yes choice
+        findSubsets(str, ans+str.charAt(i), i+1);
+        //no choice
+        findSubsets(str, ans, i+1);
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        String ans = "";
+        findSubsets(str, ans, 0);
+    }
+}
