a57b24eff57e2b8b4960b25109fbfdad362f56c1
Kritika Singh <kritikasingh2736@gmail.com>
2025-10-30 21:59:53 +0530

    reverse a string using stack

--- StacksDS/StackC.java
+++ StacksDS/StackC.java
@@ -3,6 +3,7 @@ package StacksDS;
 import java.util.*;
 
 public class StackC {
+    //push an element at the bottom of the string
     public static void pushAtBottom(int data, Stack<Integer> s){
         //base case
         if(s.isEmpty()){
@@ -13,6 +14,24 @@ public class StackC {
         int top = s.pop();
         pushAtBottom(data, s);
         s.push(top);
+    }
+
+    //Reverse a string using a stack
+    public static String reverseString(String str){
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while(idx < str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+
+        StringBuilder result = new StringBuilder();
+        while(!s.isEmpty()){
+            char curr = s.pop();
+            result.append(curr);
+        }
+
+        return result.toString();
     }
 
     public static void main(String[] args) {
@@ -28,5 +47,8 @@ public class StackC {
             System.out.println(s.peek());
             s.pop();
         }
+
+        String str = "abc";
+        System.out.println("The reversed string is : " + reverseString(str));
     }
 }
