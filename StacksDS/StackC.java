e27820126612cac82595cafe43307aff12dbdc14
Kritika Singh <kritikasingh2736@gmail.com>
2025-11-01 19:39:38 +0530

    Stock span problem

--- StacksDS/StackC.java
+++ StacksDS/StackC.java
@@ -45,6 +45,28 @@ public class StackC {
         return result.toString();
     }
 
+
+    public static void stockSpan(int stocks[], int span[]){
+        Stack<Integer>s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+
+        for(int i = 1; i<stocks.length; i++){
+            int  currPrice = stocks[i];
+            while (!s.isEmpty() && currPrice > stocks[s.peek()]) {
+                s.pop();
+            }
+            if(s.isEmpty()){
+                span[i] = i+1;
+            }
+            else{
+                int prevHigh = s.peek();
+                span[i] = i - prevHigh;
+            }
+            s.push(i);
+        }
+    }
+
     public static void printStack(Stack<Integer>s){
         while(!s.isEmpty()){
             System.out.println(s.peek());
@@ -66,5 +88,13 @@ public class StackC {
 
         String str = "abc";
         System.out.println("The reversed string is : " + reverseString(str));
+
+        int stocks[] = {100,80,60,70,60,85,100};
+        int span[] = new int[stocks.length];
+        stockSpan(stocks, span);
+        
+        for(int i=0; i<span.length; i++){
+            System.out.print(span[i] + " ");
+        }
     }
 }
